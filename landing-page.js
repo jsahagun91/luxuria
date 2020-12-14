a59e16f3932356c5b4ff5d8bde3c228147fbecf5
Jose Sahagun <josesahagun@Joses-MacBook-Pro.local>
2020-12-14 15:36:57 -0800

    first

---
+++ landing-page.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Setting up the public directory
+app.use(express.static('landing-page'));
+
+app.get('/', (req, res) => {
+    res.sendFile('./landing-page/home.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./landing-page/about.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile('./landing-page/contact.html', { root: __dirname });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Setting up the public directory
+app.use('/public', express.static('landing-page'));
+
+app.get('/', (req, res) => {
+    res.sendFile('./landing-page/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./landing-page/about.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile('./landing-page/contact.html', { root: __dirname });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));

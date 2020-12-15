5132f2a69d3e4b716c680ddfcf2607126ff4fda3
Jose Sahagun <josesahagun@Joses-MacBook-Pro.local>
2020-12-14 16:02:33 -0800

    2

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 
 // Setting up the public directory
+
+app.use('/', express.static('landing-page'));
+
 app.use('/public', express.static('landing-page'));
 
 app.get('/', (req, res) => {
